a962669f0d754b842af9a2c89ec2ed7dc9364f33
Michael Tran <michaelhtran120@gmail.com>
2021-10-12 14:26:15 -0700

    minor cleanup and refactor js using jquery

--- script.js
+++ script.js
@@ -30,104 +30,83 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("orientationChange", lazyLoad);
 });
 
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+$(window).scroll(function () {
   scrollFunction();
-};
+});
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  if ($(document).scrollTop() > 20) {
+    $("#btn-back-to-top").css("display", "block");
   } else {
-    mybutton.style.display = "none";
+    $("#btn-back-to-top").css("display", "none");
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+$("#btn-back-to-top").click(backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  $(document).scrollTop(0);
 }
 
 // Sign Up Form Check
 
-let errorEmailP = document.getElementById("error-email-p");
-let errorPasswordP = document.getElementById("error-password-p");
-let signUpBtn = document.getElementById("signUp-btn");
-
-signUpBtn.disabled = true;
-
-let emailInput = document.getElementById("signUpEmail");
-let confirmEmailInput = document.getElementById("confirmSignUpEmail");
-
-let passwordInput = document.getElementById("signUpPassword");
-let confirmPasswordInput = document.getElementById("confirmSignUpPassword");
-
 let borderErrStyle = "1px solid red";
 let borderStyle = "1px solid #ced4da";
 
-// Email check
+$("#signUpBtn").prop("disabled", true);
 
-confirmEmailInput.addEventListener("change", () => {
-  if (emailInput.value !== confirmEmailInput.value) {
-    errorEmailP.textContent = `Email's do not match`;
-    emailInput.style.border = borderErrStyle;
-    confirmEmailInput.style.border = borderErrStyle;
-    signUpBtn.disabled = true;
+// Form - Email check for sign up
+$("#confirmSignUpEmail").change(function () {
+  if ($("#signUpEmail").val() !== $(this).val()) {
+    $("#errorEmailP").text(`Email's do not match`);
+    $("#signUpEmail").css("border", borderErrStyle);
+    $(this).css("border", borderErrStyle);
   } else {
-    errorEmailP.textContent = "";
-    emailInput.style.border = borderStyle;
-    confirmEmailInput.style.border = borderStyle;
+    $("errorEmailP").text("");
+    $("#signUpEmail").css("border", borderStyle);
+    $(this).css("border", borderStyle);
     if (
-      passwordInput.value === confirmPasswordInput.value &&
-      passwordInput.value !== "" &&
-      confirmPasswordInput !== ""
+      $("#signUpPassword").val() === $("#confirmSignUpPassword") &&
+      $("#signUpPassword").val() !== "" &&
+      $(this).val() !== ""
     ) {
-      signUpBtn.disabled = false;
+      $("#signUpBtn").prop("disabled", false);
     }
   }
 });
 
-// Password check
+// Password check logic
+// Password check length on keypress
 
-// Password check length on keystroke
-
-passwordInput.addEventListener("keypress", () => {
-  let passwordValue = document.getElementById("signUpPassword").value;
-  if (passwordValue.length < 8) {
-    errorPasswordP.textContent = "Password too short - Minimum 8 characters ";
+$("#signUpPassword").keypress(function () {
+  console.log($(this).val().length);
+  if ($(this).val().length > 6) {
+    $("#errorPasswordP").text(" ");
   } else {
-    errorPasswordP.textContent = "";
+    $("#errorPasswordP").text("Password too short - Minimum 8 characters");
   }
 });
 
 // Password check confirmation on change.
 
-confirmPasswordInput.addEventListener("change", () => {
-  if (passwordInput.value !== confirmPasswordInput.value) {
-    errorPasswordP.textContent += `Password's do not match `;
-    passwordInput.style.border = borderErrStyle;
-    confirmPasswordInput.style.border = borderErrStyle;
-    signUpBtn.disabled = true;
+$("#confirmSignUpPassword").change(function () {
+  if ($(this).val() !== $("#signUpPassword").val()) {
+    $("#errorPasswordP").text(`Passwords do not match`);
+    $(this).css("border", borderErrStyle);
+    $("#signUpPassword").css("border", borderErrStyle);
+    $("#signUpBtn").prop("disabled", true);
   } else {
-    errorPasswordP.textContent = "";
-    passwordInput.style.border = borderStyle;
-    confirmPasswordInput.style.border = borderStyle;
+    $("#errorPasswordP").text("");
+    $(this).css("border", borderStyle);
+    $("#signUpPassword").css("border", borderStyle);
     if (
-      emailInput.value === confirmEmailInput.value &&
-      emailInput.value !== "" &&
-      confirmEmailInput !== ""
+      $("#signUpEmail").val() === $("#confirmSignUpEmail").val() &&
+      $("#signUpEmail").val() !== "" &&
+      $("#confirmSignUpEmail").val() !== ""
     ) {
-      signUpBtn.disabled = false;
+      $("#signUpBtn").prop("disabled", false);
     }
   }
 });
-
-window.onload = () => {
-  console.log("page loaded");
-};
